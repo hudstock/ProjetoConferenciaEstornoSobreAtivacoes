95aa2bed554e63b83edc088569ae520ccbe3799b
Hudson Pires <hudstock@gmail.com>
2022-05-24 16:39:33 -0300

    Gerando Projeto Spring Boot
    Criando projeto Spring Boot

---
+++ src/main/java/com/example/conferenciaestorno/ConferenciaEstornoApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferenciaestorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenciaEstornoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenciaEstornoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conferenciaestorno/ConferenciaEstornoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferenciaestorno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenciaEstornoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
