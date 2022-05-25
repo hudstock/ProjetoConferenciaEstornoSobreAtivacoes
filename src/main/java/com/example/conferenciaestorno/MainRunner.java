925e4c5b560eb7e483e253a2274a21ffafd4a0f5
Hudson Pires <hudstock@gmail.com>
2022-05-25 18:14:41 -0300

    Acertando exemplo para trabalhar corretamente com Optional
    Assim ficará de exemplo para demais consultas. Melhor retornar o Optional pelo service tbm.

--- src/main/java/com/example/conferenciaestorno/MainRunner.java
+++ src/main/java/com/example/conferenciaestorno/MainRunner.java
@@ -12,47 +12,54 @@ import com.example.conferenciaestorno.domain.model.TipoLancamentoEnum;
 import com.example.conferenciaestorno.domain.repository.LancamentoRepository;
 
 @Configuration
-public class MainRunner implements CommandLineRunner{
-	
+public class MainRunner implements CommandLineRunner {
+
 	@Autowired
 	LancamentoRepository lancamentoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Starting Main Runner");
-		
+		testeAmbiente();
+
+	}
+
+	private void testeAmbiente() {
+
 		Optional<Lancamento> result = lancamentoRepository.findById(1L);
-		
+
 		System.out.println("Buscando Lancamento com id = 1");
-		System.out.println(result.get());
-		
+		if (result.isPresent()){
+			System.out.println(result.get());
+		} else {
+			System.out.println("Lançamento id não encontrado");
+		}
+
 		System.out.println("Buscando Lancamento com Pedido = 1");
-		List<Lancamento> results = lancamentoRepository.findAllByPedido("1");		
+		List<Lancamento> results = lancamentoRepository.findAllByPedido("1");
 		results.stream().forEach((Lancamento lancamento) -> {
-		    System.out.println(lancamento);
+			System.out.println(lancamento);
 		});
-		
+
 		System.out.println("Buscando Lancamento com Pedido = 1");
-		List<Lancamento> results2 = lancamentoRepository.findAllByCnpjCpf("456");		
+		List<Lancamento> results2 = lancamentoRepository.findAllByCnpjCpf("456");
 		results2.stream().forEach((Lancamento lancamento) -> {
-		    System.out.println(lancamento);
+			System.out.println(lancamento);
 		});
-		
+
 		Lancamento novoLancamento = new Lancamento();
-		
+
 		novoLancamento.setCnpjCpf("teste");
 		novoLancamento.setPedido("teste");
 		novoLancamento.setTipoLancamento(TipoLancamentoEnum.ESTORNO);
-		
+
 		lancamentoRepository.save(novoLancamento);
-		
+
 		System.out.println("Buscando Lancamento com Pedido = teste");
-		List<Lancamento> results3 = lancamentoRepository.findAllByPedido("teste");		
+		List<Lancamento> results3 = lancamentoRepository.findAllByPedido("teste");
 		results3.stream().forEach((Lancamento lancamento) -> {
-		    System.out.println(lancamento);
+			System.out.println(lancamento);
 		});
-		
-		
 
 	}
 
